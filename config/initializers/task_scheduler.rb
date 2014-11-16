9f8391d9f07cf90f537a868f3e75c5fcc12012a6
Alexander Dobriakov <adobriakov@gmail.com>
2014-11-16 20:10:35 +0100

    Add table with 3 indexes

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler.every("15m") do
 	# byebug
 	quotes = StockQuote::Stock.quote('^GSPC, ^N225, ^GDAXI')
 	quotes.each do |q| 
-	  logger.debug "#{DateTime.now}: #{q.symbol}, #{q.previous_close}, #{q.last_trade_price_only}"
+	  Rails.logger.debug "#{DateTime.now}: #{q.symbol}, #{q.previous_close}, #{q.last_trade_price_only}"
 	  Quote.create(symbol: q.symbol, previous_close: q.previous_close, last_trade_price_only: q.last_trade_price_only)
 	end
 end 
